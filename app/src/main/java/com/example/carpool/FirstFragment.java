b9aaa142961da9f76fdac9762232e99df567cb28
MatioS <dkm2@hi.is>
2025-02-15 19:48:10 +0000

    Revert "feat: implement API connection between mobile app and backend"
    This reverts commit 63420b58fc95a77dd65db6ee2b053e3029580e07.

--- app/src/main/java/com/example/carpool/FirstFragment.java
+++ app/src/main/java/com/example/carpool/FirstFragment.java
@@ -1,11 +1,5 @@
 package com.example.carpool;
 
-import com.example.carpool.network.ApiService; 
-import com.example.carpool.network.RetrofitClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import java.util.List;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,24 +16,14 @@ public class FirstFragment extends Fragment {
     private FragmentFirstBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
-        ApiService api = RetrofitClient.getApi();
-        api.getAllOffers().enqueue(new Callback<List<Object>>() {
-            @Override
-            public void onResponse(Call<List<Object>> call, Response<List<Object>> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    // TODO: Handle API data 
-                }
-            }
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
 
-            @Override
-            public void onFailure(Call<List<Object>> call, Throwable t) {
-                // TODO:Show error message
-            }
-        });
-        return view;
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
