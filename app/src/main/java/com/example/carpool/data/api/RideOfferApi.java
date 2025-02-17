9e77f1ac289f2c8fdf8bcb1319b77fb0653afbbe
SysMac5 <sigurthor.maggi@gmail.com>
2025-02-17 15:51:04 +0000

    create Service interface post for create ride offer

--- app/src/main/java/com/example/carpool/data/api/RideOfferApi.java
+++ app/src/main/java/com/example/carpool/data/api/RideOfferApi.java
@@ -1,10 +1,14 @@
 package com.example.carpool.data.api;
 
 import com.example.carpool.data.models.PageResponse;
+import com.example.carpool.data.models.RideOfferRequest;
 import com.example.carpool.data.models.RideOfferResponse;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface RideOfferApi {
@@ -12,5 +16,10 @@ public interface RideOfferApi {
     Call<PageResponse<RideOfferResponse>> getPaginatedOffers(
             @Query("page") int page,
             @Query("size") int size
+    );
+
+    @POST("offers/create")
+    Call<ResponseBody> createRideOffer(
+            @Body RideOfferRequest rideOfferRequest
     );
 }
