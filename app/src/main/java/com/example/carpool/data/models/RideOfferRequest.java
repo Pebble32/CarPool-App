8b0c86854542b296e6cae3a8de7b1fd877d8f156
SysMac5 <sigurthor.maggi@gmail.com>
2025-02-24 16:41:34 +0000

    ride offer request ensures ISO format

--- app/src/main/java/com/example/carpool/data/models/RideOfferRequest.java
+++ app/src/main/java/com/example/carpool/data/models/RideOfferRequest.java
@@ -1,6 +1,7 @@
 package com.example.carpool.data.models;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class RideOfferRequest {
     private String startLocation;
@@ -8,10 +9,10 @@ public class RideOfferRequest {
     private String departureTime;
     private Integer availableSeats;
 
-    public RideOfferRequest(String startLocation, String endLocation, String departureTime, Integer availableSeats) {
+    public RideOfferRequest(String startLocation, String endLocation, LocalDateTime departureTime, Integer availableSeats) {
         this.startLocation = startLocation;
         this.endLocation = endLocation;
-        this.departureTime = departureTime;
+        this.departureTime = departureTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         this.availableSeats = availableSeats;
     }
 
